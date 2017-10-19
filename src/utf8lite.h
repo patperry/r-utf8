9c510be8890ecd7473703c54b7392f6b5d990988
Patrick Perry <patperry@gmail.com>
2017-10-18 20:04:43 -0400

    merge headers; doc update

--- src/utf8lite.h
+++ src/utf8lite.h
@@ -404,8 +404,7 @@ void utf8lite_compose(int32_t *ptr, size_t *lenptr);
 /**@}*/
 
 /**
- * \defgroup text UTF-8 encoded text, optionally with JSON-compatible
- *   backslash (\\) escapes
+ * \defgroup text UTF-8 encoded text
  * @{
  */
 
@@ -611,6 +610,94 @@ void utf8lite_text_iter_reset(struct utf8lite_text_iter *it);
  * \param it the text iterator
  */
 void utf8lite_text_iter_skip(struct utf8lite_text_iter *it);
+
+/**@}*/
+
+/**
+ * \defgroup textmap Text normalization map.
+ * @{
+ */
+
+/**
+ * Map descriptor. At a minimum, convert the text to
+ * composed normal form (NFC). Optionally, apply compatibility maps for
+ * NFKC normal and/or apply other transformations:
+ *
+ *  + #UTF8LITE_TEXTMAP_CASE: perform case folding, in most languages (including
+ *  	English) mapping uppercase characters to their lowercase equivalents,
+ *  	but also performing other normalizations like mapping the
+ *  	German Eszett (&szlig;) to "ss"; see
+ *  	_The Unicode Standard_ Sec. 5.18 "Case Mappings"
+ *  	and the
+ *  	[Case Mapping FAQ](http://unicode.org/faq/casemap_charprop.html)
+ *  	for more information
+ *
+ *  + #UTF8LITE_TEXTMAP_COMPAT: apply all compatibility maps required for
+ *  	[NFKC normal form](http://unicode.org/reports/tr15/#Norm_Forms)
+ *
+ *  + #UTF8LITE_TEXTMAP_QUOTE: quote fold, replace single quotes and
+ *      Unicode apostrophe with ASCII apostrophe (U+0027)
+ *
+ *  + #UTF8LITE_TEXTMAP_RMDI: remove default ignorables (DI) like soft
+ *      hyphens and zero-width spaces, anything with the
+ *  	[Default_Ignorable_Code_Point=Yes]
+ *  	(http://www.unicode.org/reports/tr44/#Default_Ignorable_Code_Point)
+ *  	property
+ */
+enum utf8lite_textmap_type {
+	UTF8LITE_TEXTMAP_NORMAL = 0, /**< transform to composed normal form */
+	UTF8LITE_TEXTMAP_CASE   = (1 << 0), /**< perform case folding */
+	UTF8LITE_TEXTMAP_COMPAT = (1 << 1), /**< apply compatibility mappings */
+	UTF8LITE_TEXTMAP_QUOTE  = (1 << 2), /**< replace apostrophe with `'` */
+	UTF8LITE_TEXTMAP_RMDI   = (1 << 3)  /**< remove default ignorables */
+};
+
+/**
+ * Type map, for normalizing tokens to types.
+ */
+struct utf8lite_textmap {
+	struct utf8lite_text text;/**< result of the most recent call to
+				    utf8lite_textmap_set() */
+	int8_t ascii_map[128];	/**< a lookup table for the mappings of ASCII
+				  characters; -1 indicates deletion */
+	int32_t *codes;		/**< buffer for intermediate UTF-32 decoding */
+	size_t size_max;	/**< text size maximum; normalizing a larger
+				 	text will force a reallocation */
+	int type;		/**< the map type descriptor, a bit mask
+				  of #utf8lite_textmap_type values */
+	int charmap_type;	/**< the unicode map type, a bit mask of
+				  #utf8lite_decomp_type and
+				  #utf8lite_casefold_type values */
+};
+
+/**
+ * Initialize a new text map of the specified kind.
+ *
+ * \param map the text map
+ * \param type a bitmask of #utf8lite_textmap_type values, specifying
+ * 	the map type
+ *
+ * \returns 0 on success
+ */
+int utf8lite_textmap_init(struct utf8lite_textmap *map, int type);
+
+/**
+ * Release the resources associated with a text map.
+ * 
+ * \param map the text map
+ */
+void utf8lite_textmap_destroy(struct utf8lite_textmap *map);
+
+/**
+ * Given input text, set a map to the corresponding output text.
+ *
+ * \param map the text map
+ * \param text the text
+ *
+ * \returns 0 on success
+ */
+int utf8lite_textmap_set(struct utf8lite_textmap *map,
+			 const struct utf8lite_text *text);
 
 /**@}*/
 
