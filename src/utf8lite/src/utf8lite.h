98225320e24567742c77b6d8362007c01e92d7ed
Patrick Perry <patperry@gmail.com>
2017-10-24 07:29:46 -0400

    use format attribute for clang on windows

--- src/utf8lite/src/utf8lite.h
+++ src/utf8lite/src/utf8lite.h
@@ -71,7 +71,7 @@ void utf8lite_message_clear(struct utf8lite_message *msg);
  * \param ... format arguments
  */
 void utf8lite_message_set(struct utf8lite_message *msg, const char *fmt, ...)
-#if defined(_MSC_VER)
+#if defined(_MSC_VER) && !defined(__clang__)
 	;
 #else
 	__attribute__ ((format (printf, 2, 3)));
@@ -85,7 +85,7 @@ void utf8lite_message_set(struct utf8lite_message *msg, const char *fmt, ...)
  * \param ... format arguments
  */
 void utf8lite_message_append(struct utf8lite_message *msg, const char *fmt, ...)
-#if defined(_MSC_VER)
+#if defined(_MSC_VER) && !defined(__clang__)
 	;
 #else
 	__attribute__ ((format (printf, 2, 3)));
@@ -902,7 +902,7 @@ int utf8lite_render_string(struct utf8lite_render *r, const char *str);
  * \param format the format string
  */
 int utf8lite_render_printf(struct utf8lite_render *r, const char *format, ...)
-#if defined(_MSC_VER)
+#if defined(_MSC_VER) && !defined(__clang__)
 	;
 #else
 	__attribute__ ((format (printf, 2, 3)));
