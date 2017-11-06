ac0484eeef782d4cc083042791bfa422f0b0a83a
Patrick Perry <patperry@gmail.com>
2017-11-06 13:14:48 -0500

    don't export 'rutf8_translate_utf8'

--- src/init.c
+++ src/init.c
@@ -36,8 +36,6 @@ void R_init_utf8(DllInfo *dll)
 {
 	R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
 	R_RegisterCCallable("utf8", "as_utf8", (DL_FUNC)&rutf8_as_utf8);
-	R_RegisterCCallable("utf8", "translate_utf8",
-			    (DL_FUNC)&rutf8_translate_utf8);
 	R_useDynamicSymbols(dll, FALSE);
 	R_forceSymbols(dll, TRUE);
 }
